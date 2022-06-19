67d0f52380b5113b591e6d8f87ba0f5fba6506f6
Factzure Enterprise <factzure@hotmail.com>
2022-06-19 17:21:09 -0500

    Agregar archivos de proyecto.

---
+++ Projecto19/Controllers/ClientesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projecto19.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Listado()
+        {
+            return View();
+        }
+        public ActionResult Alta()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projecto19/Controllers/ProveedoresController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projecto19.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        // GET: Proveedores
+        public ActionResult Listado()
+        {
+            return View();
+        }
+        public ActionResult Alta()
+        {
+            return View();
+        }
+    }
+}
